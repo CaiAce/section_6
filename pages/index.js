86760ade2688ea10c7d5c968ccb959ccdc1da5b9
Cailean <chorton44@aceproject.space>
2023-02-03 11:25:52 -0600

    Section 7 complete
    Added all optimizations and completed all tasks in section 7.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 function HomePage(props) {
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Meta description tag!!" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
